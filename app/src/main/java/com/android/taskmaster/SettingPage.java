09cb37abdeadba4057bc33ac2fd798692381112e
judyal-taweel <judybataineh@gmail.com>
2021-08-22 23:23:07 +0300

    lab36

--- app/src/main/java/com/android/taskmaster/SettingPage.java
+++ app/src/main/java/com/android/taskmaster/SettingPage.java
@@ -5,11 +5,17 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class SettingPage extends AppCompatActivity {
+
+    String theTeam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +24,36 @@ public class SettingPage extends AppCompatActivity {
         setContentView(R.layout.activity_setting_page);
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor prefEditor = sharedPreferences.edit();
+
+        Spinner spinner2 =  findViewById(R.id.spinnerForTeam);
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.team_menu, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                theTeam = (String) parent.getItemAtPosition(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                theTeam = (String) parent.getItemAtPosition(0);
+
+            }
+        });
+
         Button saveBtn = findViewById(R.id.save_username_btn);
         saveBtn.setOnClickListener(v -> {
             String username = ((EditText)findViewById(R.id.username_edit_txt)).getText().toString();
 
             prefEditor.putString("Username",username);
+            prefEditor.putString("team",theTeam);
+
             prefEditor.apply();
             Toast.makeText(SettingPage.this,"username updated",Toast.LENGTH_LONG).show();
         });
